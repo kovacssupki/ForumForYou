54a11b21772d9064d6abf5048a4b9cef9a2de546
chirastefan <chirastefan@yahoo.com>
2016-01-20 11:25:56 +0200

    dropdown hover
    activate dropdown on hovver(connect button)
    border arround menu buttons(on hover)

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,10 +1,5 @@
 $(document).ready(function () {
-    $('#connect').hover(
-        function () {
-            $('.dropdown-menu').stop(true,true).slideDown('medium');
-        },
-        function () {
-            $('.dropdown-menu').stop(true,true).slideUp('fast');
-        }
+    $('#connect').hover(function () {
+            $('.dropdown-menu').stop(true).slideToggle(300);
     });
 });
