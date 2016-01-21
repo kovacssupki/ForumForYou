b7e396550105529a565fac74fa8dc55048f17b3c
Stanca <stanca_pop@yahoo.com>
2016-01-21 16:23:26 +0200

    created dropdown directive, deleted bower folder

--- src/js/app.js
+++ src/js/app.js
@@ -1,26 +1,5 @@
-angular.module('Blog',['ui.router','ui.bootstrap'])
-.config(['$urlRouterProvider','$stateProvider',function($urlRouterProvider,$stateProvider){
-      $stateProvider
-      .state('home',{
-          url: '/',
-          templateUrl:'views/home.html',
-          controller: 'homeCtrl'
-      })
-      .state('articles',{
-          url: '/articles',
-          templateUrl:'views/articles.html'
-      })
-      .state('viewArticle', {
-          url: '/articles/:id',
-          templateUrl: 'views/view-article.html'
-      })
-      .state('signup',{
-         url:'/signup',
-         templateUrl:'views/signup.html'
-      })
-      .state('about',{
-         url:'/about',
-         templateUrl:'views/about.html'
-      })
-       $urlRouterProvider.otherwise("/");
-   }]);
+angular
+    .module('Blog', [
+        'ui.router',
+        'ui.bootstrap'
+    ]);
