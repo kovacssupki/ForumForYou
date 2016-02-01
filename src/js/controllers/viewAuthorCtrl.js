19a783d8fd3ed2bc1bd1ee187d557125f99e0d2b
Stanca <stanca_pop@yahoo.com>
2016-02-01 18:45:14 +0200

    test changes

--- src/js/controllers/viewAuthorCtrl.js
+++ src/js/controllers/viewAuthorCtrl.js
@@ -3,11 +3,11 @@ angular
   .controller('viewAuthorCtrl', ['$scope', '$stateParams','GetRequest',
   function($scope, $stateParams,GetRequest) {
 
-      GetRequest.get_data('http://localhost/PROJECTS/github/ForumForYou/src/data/users.json').then(function(resp){
+      GetRequest.get_data('../src/data/users.json').then(function(resp){
         $scope.users = resp.data;
 
         console.log($scope.users);
-        GetRequest.get_data('http://localhost/PROJECTS/github/ForumForYou/src/data/articles.json').then(function(resp){
+        GetRequest.get_data('../src/data/articles.json').then(function(resp){
 
           $scope.art = resp.data;
 
