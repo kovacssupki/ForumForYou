c4f5aedb85f7bb0947e70efc63843fa2c702b936
Stanca <stanca_pop@yahoo.com>
2016-02-03 17:27:13 +0200

    added carousel to viewAuthor page

--- src/js/controllers/viewAuthorCtrl.js
+++ src/js/controllers/viewAuthorCtrl.js
@@ -11,11 +11,26 @@ angular
 
           $scope.art = resp.data;
 
-          for(var i=0; i<$scope.users.length; i++){
+          for (var i = 0; i < $scope.users.length; i++){
             if($scope.users[i].id === $stateParams.id){
               $scope.author = $scope.users[i];
             }
           }
       });
     });
+    $scope.myInterval = 5000;
+    $scope.noWrapSlides = false;
+    var slides = $scope.slides = [];
+
+    $scope.addSlide = function() {
+      slides.push({
+        image: ["assets/images/fun.jpg", "assets/images/book.jpg", "assets/images/bio.jpg", "assets/images/angular.jpg", "assets/images/android.jpg", "assets/images/facebook.jpg"][slides.length % 6],
+        text: ["Fun facts!", "100 Good Books to Read: Book Recommendations", "The Organic Effect...", "Learning Angular JS", "Android 6 Vs. iOS 9: The Showdown", "Breaking news about Facebook!"][slides.length % 6]
+      });
+    };
+
+    for (var i = 0; i < 6; i++) {
+      $scope.addSlide();
+    }
 }]);
+
