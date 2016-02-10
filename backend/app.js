3d0042ef8b2f0e832e2e8fae2a6d51f983551a94
chirastefan <chirastefan@yahoo.com>
2016-02-10 09:35:56 +0200

    deleted commented code from gulp file; added grunt file for grunt watch-for livereloading-not working yet

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,27 @@
 
+
   var koa = require('koa');
   var router = require('koa-router')();
-  var livereload = require('koa-livereload');
+  // var livereload = require('koa-livereload');
 
   var port = 200;
   var app = koa();
 
-  var index = require('./routes/index');
-  var about = require('./routes/about');
+  // var index = require('./routes/index');
+  // var about = require('./routes/about');
 
-  // router.get('/', function *(next) {
-  //   this.body = 'Home';
-  // });
-  // router.get('/users', function *(next) {
-  //   this.body = 'Users';
-  // });
-  router.use('/', index.routes(), index.allowedMethods());
-  router.use('/about', about.routes(), about.allowedMethods());
+  router.get('/', function *(next) {
+    this.body = 'Home';
+  });
+  router.get('/about', function *(next) {
+    this.body = 'about';
+  });
+  // router.use('/', index.routes(), index.allowedMethods());
+  // router.use('/about', about.routes(), about.allowedMethods());
 
-  app.use(livereload());
+  // app.use(livereload());
   app.use(router.routes());
+  app.use(router.allowedMethods());
 
 
   app.listen(port);
