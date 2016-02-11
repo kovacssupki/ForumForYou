593a4a133547fd7bfa2789bbdb8022e1c4454743
chirastefan <chirastefan@yahoo.com>
2016-02-11 16:22:44 +0200

    changed meniu bar-introduced search input in navbar header;added funtctional routing for articles id

---
+++ backend/controllers/articles.js
@@ -0,0 +1,19 @@
+'use strict';
+ var fs = require("co-fs");
+
+exports = module.exports = ( ) => {
+    return function* ( ) {
+
+        var articles = JSON.parse(yield fs.readFile('models/articles.json', 'utf8')).articles;
+        this.success(
+          {
+          articles:articles
+          }
+        );
+    };
+};
+
+exports[ 'singleton' ] = true;
+// exports[ '@require' ] = [
+//    'node_modules/co-fs'
+// ];
